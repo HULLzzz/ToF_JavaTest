dd8591cb5d52814dfcda9a676bcedfdf5002c9e4
Hull <34482512+HULLzzz@users.noreply.github.com>
2018-12-21 14:50:05 +0800

    Update Test_43_SumOfSifter.java

--- ToF/com/ToF/test/Test_43_SumOfSifter.java
+++ ToF/com/ToF/test/Test_43_SumOfSifter.java
@@ -25,4 +25,41 @@ public class Test_43_SumOfSifter {
         int maxSum = number*max;
         int[] probabilities = new int[maxSum - number + 1];
     }
+    
+    
+    //循环解法
+    public static void solution43(int num){
+        if (num<1)
+            return;
+        int[][] probabilities = new int[2][6*num + 1];
+        for (int i = 0;i<6;i++){
+            probabilities[0][i] = 0;
+            probabilities[1][i] = 0;
+        }
+        int flag = 0;
+        for (int i = 1;i<=6;i++){
+            //第一次抛筛子的时候有6种可能，每种可能出现一次
+            probabilities[flag][i] = 1;
+        }
+        //从第2次开始掷筛子，假设第一个数组中的第n个数字标识筛子和为n出现的次数
+        //下一个循环的时候，加上一个新的筛子，这样此时和为n的筛子出现的次数应该是上一次
+        //筛子点数为n-1，n-2...n-6的次数总和，所以我们将另一个数组设为前一个数组对应的n-1 n-2...n-6之和
+        for (int k = 2;k<=num;k++){
+            for (int i = 0;i<k;i++){//第k次掷筛子，和最小是k，令不可能的值设置为0
+                probabilities[1-flag][i] = 0;
+            }
+            for (int i = k;i<=6*num;i++){
+                probabilities[1-flag][i] = 0; //初始化，因为这是一个反复使用的数组，需要将上次的结果清0
+                for (int j = 1;j<=i&&j<=6;j++){
+                    probabilities[1-flag][i] += probabilities[flag][i-j];
+                }
+            }
+            flag = 1-flag;
+        }
+        double total = Math.pow(6,num);
+        for (int i = num;i<6*num;i++){
+            double ratio = probabilities[flag][i]/total;
+            System.out.println("sum"+i+"ratio"+ratio);
+        }
+    }
 }
